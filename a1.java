95f0927657b14dfe69018288a0dbf9bdce1e5f41
mohammed <mohammed03074@gmail.com>
2024-12-08 19:45:01 +0530

    first commit

---
+++ a1.java
@@ -0,0 +1,7 @@
+public class a1{
+    public static void main(String []args){
+        System.out.println("hello world");
+             System.out.println("hello mysore");
+             System.out.println("hello man how are yoye");
+    }
+}
